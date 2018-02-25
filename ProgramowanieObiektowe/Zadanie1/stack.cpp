70eb497b6d9d30a1a0c4279184fea818a80535ef
Wojciech Mojsiejuk <wojciechmojsiejuk@gmail.com>
2018-02-25 16:06:24 +0100

    Update stack.cpp

--- ProgramowanieObiektowe/Zadanie1/stack.cpp
+++ ProgramowanieObiektowe/Zadanie1/stack.cpp
@@ -6,18 +6,40 @@
 //  Copyright © 2018 Wojciech Mojsiejuk. All rights reserved.
 //
 #include <iostream>
-#define STACK_SIZE 10
+#include <cstdlib>
+#include <ctime>
+#define STACK_SIZE 5
 struct STACK
 {
     int number;
     struct STACK *next;
 };
-//1
 STACK* InitStack()
 {
     STACK* newstack  = new STACK;
     newstack->next=NULL;
     return newstack;
+}
+bool IsEmpty(STACK* Bottom)
+{
+    //Condition checks whether stack was not initialized or was initialized but did not receive any value - by default it gets 0
+    if(Bottom==NULL or (Bottom->next==NULL and Bottom->number==0))
+        return true;
+    return false;
+}
+bool IsFull(STACK* Bottom)
+{
+    int counter=0;
+    while(Bottom->next!=NULL)
+    {
+        Bottom=Bottom->next;
+        counter++;
+        if(counter>=STACK_SIZE)
+        {
+            return true;
+        }
+    }
+    return false;
 }
 void DestroyStack(STACK** StackToDelete)
 {
@@ -39,7 +61,7 @@ STACK* PushStack(STACK* Bottom, int value)
         if(counter>=STACK_SIZE)
         {
             fprintf(stderr, "Stack size limit. Cannot add another value.\n");
-            return Bottom;
+            return NULL;
         }
     }
     Bottom->next=new STACK;
@@ -52,6 +74,11 @@ int PopStack(STACK* Bottom)
 {
     int pom;
     STACK *temp;
+    if(IsEmpty(Bottom)==1)
+    {
+        fprintf(stderr, "Stack is empty, cannot pop object\n");
+        return -1;
+    }
     while(Bottom->next->next!=NULL)
     {
         Bottom=Bottom->next;
@@ -70,36 +97,32 @@ void TopStack(STACK* Bottom)
     }
     std::cout<<Bottom->number<<"\n";
 }
-bool IsEmpty(STACK* Bottom)
-{
-    if(Bottom==NULL or (Bottom->next==NULL and Bottom->number==0))
-        return true;
-    return false;
-}
-bool IsFull(STACK* Bottom)
-{
-    int counter=0;
-    while(Bottom->next!=NULL)
-    {
-        Bottom=Bottom->next;
-        counter++;
-        if(counter>=STACK_SIZE)
-        {
-            return true;
-        }
-    }
-    return false;
-}
 int main(int argc, const char * argv[]) {
     STACK* init = InitStack();
-    init->number=5;
-    std::cout << init->number<<"\n";
-    //DestroyStack(&init);
-    PushStack(init, 6);
-    PushStack(init, 7);
+    std::cout<<"Czy stack jest pusty? "<<IsEmpty(init)<<"\n";
+    int i,j;
+    srand(time(NULL));
+    int limit = std::rand()%10;
+    int value;
+    for(i=0;i<limit;i++)
+    {
+        value= std::rand()%20;
+        if(PushStack(init, value)==NULL)
+        {
+            break;
+        }
+        std::cout<<i<<": "<<value<<"\n";
+    }
+    std::cout<<"Czy stack jest pusty? "<<IsEmpty(init)<<"\n";
+    std::cout<<"Czy stack jest pełny? "<<IsFull(init)<<"\n";
+    std::cout<<"\nTop element: ";
     TopStack(init);
-    //int wynik = PopStack(init);
-    //std::cout << wynik << init->number<<"\n";
-    //to fix: delete doesnt work
+    std::cout<<"\nWartosci stosu zdejmowane z gory: \n";
+    
+    for(j=0;j<i;j++)
+    {
+        std::cout<<PopStack(init)<<"\n";
+    }
+    DestroyStack(&init);
     return 0;
 }
