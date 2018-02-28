ad7e97e41a5cd3e831e698c56b33a7337030bb8e
pa-b <33585264+pa-b@users.noreply.github.com>
2018-02-28 23:11:26 +0100

    Add files via upload

---
+++ ProgramowanieObiektowe/Zadanie1/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+
+struct stos
+{
+	int liczba;
+	stos* nast;
+};
+typedef stos* Adres;
+
+Adres init();
+void destroy(Adres&);
+void push(int, Adres&);
+void pop(Adres&);
+int top(Adres);
+int empty(Adres);
+int full();
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	Adres nowy = init();
+	int liczba = rand() % 100;
+	for(int i = 0; i < liczba; i++)
+	{
+		push(rand() % 1000, nowy);
+	}
+	while (!empty(nowy))
+	{
+		std::cout << top(nowy) << '\n';
+		pop(nowy);
+	}
+	std::cin.get();
+	return 0;
+}
+
+Adres init()
+{
+	return nullptr;
+}
+
+void destroy(Adres &pm)
+{
+	if (pm)
+	{
+		destroy(pm->nast);
+		delete pm;
+		pm = nullptr;
+	}
+}
+
+void push(int n, Adres &pm)
+{
+	Adres nowy = new stos;
+	nowy->liczba = n;
+	nowy->nast = pm;
+	pm = nowy;
+}
+
+void pop(Adres &pm)
+{
+	Adres pom = pm->nast;
+	delete pm;
+	pm = pom;
+}
+
+int top(Adres pm)
+{
+	return pm->liczba;
+}
+
+int empty(Adres pm)
+{
+	return ((pm) ? 0 : 1);
+}
+
+int full()
+{
+	return 0;
+}
