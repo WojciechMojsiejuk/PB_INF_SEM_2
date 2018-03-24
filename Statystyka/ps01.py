77d48742cb902ee20ce1c44dbebb6dbe4575e73c
Wojciech Mojsiejuk <wojciechmojsiejuk@gmail.com>
2018-03-24 19:25:41 +0100

    Update ps01.py

--- Statystyka/ps01.py
+++ Statystyka/ps01.py
@@ -2,23 +2,23 @@ import random
 import numpy as np
 import statistics as st
 import matplotlib.pyplot as plt
+
 data = []
 miejsce = [] #0 - wies 1 - miasto
 dochod = []
 czas_pracy = []
-for i in range(60):
-    if i<30: miejsce.append(1)
+for i in range(100):
+    if i<=49: miejsce.append(1)
     else: miejsce.append(0)
     if miejsce[-1]==0:
         dochod.append(random.randrange(1500,3000))
     else:
         dochod.append(random.randrange(1800,4000))
     if dochod[-1] in range(1500,2200):
-        czas_pracy.append(round(random.uniform(6,9),0))
+        czas_pracy.append(random.randrange(6,9))
     else:
-        czas_pracy.append(round(random.uniform(8,11),0))
-data = [(miejsce[i], dochod[i], czas_pracy[i]) for i in range(60)]
-print(data)
+        czas_pracy.append(random.randrange(8,11))
+data = [(miejsce[i], dochod[i], czas_pracy[i]) for i in range(100)]
 
 min_dochod = min(dochod)
 max_dochod = max(dochod)
@@ -66,16 +66,16 @@ print(
 
 
 
-min_dochod = min(dochod[30:])
-max_dochod = max(dochod[30:])
-min_czas_pracy = min(czas_pracy[30:])
-max_czas_pracy = max(czas_pracy[30:])
+min_dochod = min(dochod[50:])
+max_dochod = max(dochod[50:])
+min_czas_pracy = min(czas_pracy[50:])
+max_czas_pracy = max(czas_pracy[50:])
 range_dochod = max_dochod - min_dochod
 range_czas_pracy = max_czas_pracy - min_czas_pracy
-mean_dochod = round(np.mean(dochod[30:]),2)
-mean_czas_pracy = round(np.mean(czas_pracy[30:]),2)
-median_dochod = st.median(dochod[30:])
-median_czas = st.median(czas_pracy[30:])
+mean_dochod = round(np.mean(dochod[50:]),2)
+mean_czas_pracy = round(np.mean(czas_pracy[50:]),2)
+median_dochod = st.median(dochod[50:])
+median_czas = st.median(czas_pracy[50:])
 
 print("\nWies:\n")
 print(
@@ -87,18 +87,18 @@ print(
 mode_dochod=-1
 mode_czas=-1
 try:
-    mode_dochod = st.mode(dochod[30:])
-    mode_czas = st.mode(czas_pracy[30:])
+    mode_dochod = st.mode(dochod[50:])
+    mode_czas = st.mode(czas_pracy[50:])
 except st.StatisticsError:
     print ("\nNo unique mode found\n")
-variance_dochod = round(st.variance(dochod[30:]),2)
-variance_czas = round(st.variance(czas_pracy[30:]),2)
-stdev_dochod = round(st.stdev(dochod[30:]),2)
-stdev_czas = round(st.stdev(czas_pracy[30:]),2)
-q1_dochod = np.percentile(dochod[30:],25)
-q1_czas = np.percentile(czas_pracy[30:],25)
-q3_dochod = np.percentile(dochod[30:],75)
-q3_czas = np.percentile(czas_pracy[30:],75)
+variance_dochod = round(st.variance(dochod[50:]),2)
+variance_czas = round(st.variance(czas_pracy[50:]),2)
+stdev_dochod = round(st.stdev(dochod[50:]),2)
+stdev_czas = round(st.stdev(czas_pracy[50:]),2)
+q1_dochod = np.percentile(dochod[50:],25)
+q1_czas = np.percentile(czas_pracy[50:],25)
+q3_dochod = np.percentile(dochod[50:],75)
+q3_czas = np.percentile(czas_pracy[50:],75)
 iqr_dochod = q3_dochod-q1_dochod
 iqr_czas = q3_czas-q1_czas
 print(
@@ -112,16 +112,16 @@ print(
 
 
 
-min_dochod = min(dochod[:29])
-max_dochod = max(dochod[:29])
-min_czas_pracy = min(czas_pracy[:29])
-max_czas_pracy = max(czas_pracy[:29])
+min_dochod = min(dochod[:50])
+max_dochod = max(dochod[:50])
+min_czas_pracy = min(czas_pracy[:50])
+max_czas_pracy = max(czas_pracy[:50])
 range_dochod = max_dochod - min_dochod
 range_czas_pracy = max_czas_pracy - min_czas_pracy
-mean_dochod = round(np.mean(dochod[:29]),2)
-mean_czas_pracy = round(np.mean(czas_pracy[:29]),2)
-median_dochod = st.median(dochod[:29])
-median_czas = st.median(czas_pracy[:29])
+mean_dochod = round(np.mean(dochod[:50]),2)
+mean_czas_pracy = round(np.mean(czas_pracy[:50]),2)
+median_dochod = st.median(dochod[:50])
+median_czas = st.median(czas_pracy[:50])
 
 print("\nMiasto:\n")
 print(
@@ -133,18 +133,18 @@ print(
 mode_dochod=-1
 mode_czas=-1
 try:
-    mode_dochod = st.mode(dochod[:29])
-    mode_czas = st.mode(czas_pracy[:29])
+    mode_dochod = st.mode(dochod[:50])
+    mode_czas = st.mode(czas_pracy[:50])
 except st.StatisticsError:
     print ("No unique mode found")
-variance_dochod = round(st.variance(dochod[:29]),2)
-variance_czas = round(st.variance(czas_pracy[:29]),2)
-stdev_dochod = round(st.stdev(dochod[:29]),2)
-stdev_czas = round(st.stdev(czas_pracy[:29]),2)
-q1_dochod = np.percentile(dochod[:29],25)
-q1_czas = np.percentile(czas_pracy[:29],25)
-q3_dochod = np.percentile(dochod[:29],75)
-q3_czas = np.percentile(czas_pracy[:29],75)
+variance_dochod = round(st.variance(dochod[:50]),2)
+variance_czas = round(st.variance(czas_pracy[:50]),2)
+stdev_dochod = round(st.stdev(dochod[:50]),2)
+stdev_czas = round(st.stdev(czas_pracy[:50]),2)
+q1_dochod = np.percentile(dochod[:50],25)
+q1_czas = np.percentile(czas_pracy[:50],25)
+q3_dochod = np.percentile(dochod[:50],75)
+q3_czas = np.percentile(czas_pracy[:50],75)
 iqr_dochod = q3_dochod-q1_dochod
 iqr_czas = q3_czas-q1_czas
 print(
@@ -155,27 +155,122 @@ print(
     "\tKwartyl gorny dochodu: ",q3_dochod,"\t\t\t\tKwartyl gorny czasu: ",q3_czas,"\n"
     "\r Roz kwartylowy dochodu: ",iqr_dochod,"\t\t\t\tRoz kwartylowy czasu: ",iqr_czas,"\n"
 )
+f = open('dane.txt','w')
+for i in range(100):
+    line = str(miejsce[i])+","+str(dochod[i])+","+str(czas_pracy[i])+"\n"
+    f.writelines(line)
+f.close()
+bx1_dochod = [dochod[:50]]
+plt.boxplot(bx1_dochod)
+#"1 dochody w mieście"
+plt.title("Boxplot dochodow w miescie")
+plt.show()
 
+bx2_dochod = [dochod[50:]]
+plt.boxplot(bx2_dochod)
+#"1 dochody na wsi"
+plt.title("Boxplot dochodow na wsi")
+plt.show()
 
-bx_dochod = [dochod, dochod[:29], dochod[30:]]
+bx_dochod = [dochod[:50], dochod[50:]]
 plt.boxplot(bx_dochod)
 #"1 - dochody wszystkich 2 - dochody w mieście 3 - dochody na wsi"
-plt.title("Boxplot dochodow")
+plt.title("Boxplot dochodow na wsi i w miescie")
+plt.legend()
 plt.show()
-h_dochod = [dochod[:29], dochod[30:]]
-plt.hist(h_dochod)
+
+bx3_dochod = [dochod]
+plt.boxplot(bx3_dochod)
+#"1 - dochody wszystkich 2 - dochody w mieście 3 - dochody na wsi"
+plt.title("Boxplot dochodow (w sumie)")
+plt.legend()
+plt.show()
+
+##########################################################
+
+bins_edges=[1500,1750,2000,2250,2500,2750,3000,3250,3500,3750,4000]
+h1_dochod = [dochod[:50]]
+plt.hist(h1_dochod,bins=bins_edges)
 plt.ylabel("Liczba osób")
 plt.xlabel("Dochody (zł)")
-plt.title("Histogram dochodow")
+plt.title("Histogram dochodow w miescie")
 plt.show()
-bx_czas=[czas_pracy,czas_pracy[:29],czas_pracy[30:]]
+
+h2_dochod = [dochod[50:]]
+plt.hist(h2_dochod,bins=bins_edges)
+plt.ylabel("Liczba osób")
+plt.xlabel("Dochody (zł)")
+plt.title("Histogram dochodow na wsi")
+plt.show()
+
+h_dochod = [dochod[:50], dochod[50:]]
+plt.hist(h_dochod,bins=bins_edges)
+plt.ylabel("Liczba osób")
+plt.xlabel("Dochody (zł)")
+plt.title("Histogram dochodow na wsi i w miescie")
+plt.show()
+
+h3_dochod = [dochod]
+plt.hist(h3_dochod,bins=bins_edges)
+plt.ylabel("Liczba osób")
+plt.xlabel("Dochody (zł)")
+plt.title("Histogram dochodow (w sumie)")
+plt.show()
+
+##########################################################
+
+bx1_czas=[czas_pracy[:50]]
+plt.title("Boxplot czasu pracy w miescie")
+plt.boxplot(bx1_czas)
+plt.show()
+
+
+bx2_czas=[czas_pracy[50:]]
+plt.title("Boxplot czasu pracy na wsi")
+plt.boxplot(bx2_czas)
+plt.show()
+
+bx_czas=[czas_pracy[:50], czas_pracy[50:]]
+plt.title("Boxplot czasu pracy na wsi i w miescie")
 plt.boxplot(bx_czas)
-#"1 - dochody wszystkich 2 - dochody w mieście 3 - dochody na wsi"
-plt.title("Boxplot czasu")
 plt.show()
-h_dochod = [czas_pracy[:29], czas_pracy[30:]]
-plt.hist(h_dochod)
+
+bx3_czas=[czas_pracy]
+plt.title("Boxplot czasu pracy (w sumie)")
+plt.boxplot(bx3_czas)
+plt.show()
+
+##########################################################
+
+h1_czas = [czas_pracy[:50]]
+bins_edges=[6,7,8,9,10,11]
+plt.hist(h1_czas,bins=bins_edges,range=[6,11], density=False)
 plt.ylabel("Liczba osób")
 plt.xlabel("Czas pracy (h)")
-plt.title("Histogram czasu pracy")
+plt.title("Histogram czasu pracy w miescie")
+plt.show()
+
+h2_czas = [czas_pracy[50:]]
+bins_edges=[6,7,8,9,10,11]
+plt.hist(h2_czas,bins=bins_edges,range=[6,11], density=False)
+plt.ylabel("Liczba osób")
+plt.xlabel("Czas pracy (h)")
+plt.title("Histogram czasu pracy na wsi")
+plt.show()
+
+h_czas = [czas_pracy[:50], czas_pracy[50:]]
+bins_edges=[6,7,8,9,10,11]
+plt.hist(h_czas,bins=bins_edges,range=[6,11], density=False)
+plt.ylabel("Liczba osób")
+plt.xlabel("Czas pracy (h)")
+plt.title("Histogram czasu pracy na wsi i w miescie")
+plt.show()
+
+
+h3_czas = [czas_pracy]
+bins_edges=[6,7,8,9,10,11]
+plt.hist(h3_czas,bins=bins_edges,range=[6,11], density=False)
+plt.ylabel("Liczba osób")
+plt.xlabel("Czas pracy (h)")
+plt.title("Histogram czasu pracy (w sumie)")
 plt.show()
